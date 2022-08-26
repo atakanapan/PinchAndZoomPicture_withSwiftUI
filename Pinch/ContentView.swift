7457641f264aa49b5f53e0ea9bed036cd8772e13
atakanapan <atakanapan12@gmail.com>
2022-08-26 11:19:16 +0300

    Initial Commit

---
+++ Pinch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Atakan Apan on 8/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Atakan Apan on 8/26/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
